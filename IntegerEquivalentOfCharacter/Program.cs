e04acdbda01d390c924d11144fa4f02771afe59d
Pro <Pro@PROBOOK>
2023-03-30 21:10:57 +0500

    integer equivalent of A, B, C, a, b and c done

--- IntegerEquivalentOfCharacter/Program.cs
+++ IntegerEquivalentOfCharacter/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class C3Q27
+{
+    static void Main(string[] args)
+    {
+        // integer equivalent of characters
+        Console.WriteLine("The Character A Has The Value " + (int)'A');
+        Console.WriteLine("The Character B Has The Value " + (int)'B');
+        Console.WriteLine("The Character C Has The Value " + (int)'C');
+        Console.WriteLine("The Character a Has The Value " + (int)'a');
+        Console.WriteLine("The Character b Has The Value " + (int)'b');
+        Console.WriteLine("The Character c Has The Value " + (int)'c');
+
+    }
+}
