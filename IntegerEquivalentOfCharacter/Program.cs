fe09dc2ae716b8f6911ee5b4ea2dcb11b28e8a4e
Pro <Pro@PROBOOK>
2023-03-30 21:12:21 +0500

    solution done

--- IntegerEquivalentOfCharacter/Program.cs
+++ IntegerEquivalentOfCharacter/Program.cs
@@ -9,6 +9,12 @@
         Console.WriteLine("The Character a Has The Value " + (int)'a');
         Console.WriteLine("The Character b Has The Value " + (int)'b');
         Console.WriteLine("The Character c Has The Value " + (int)'c');
-
+        Console.WriteLine("The Character 0 Has The Value " + (int)'0');
+        Console.WriteLine("The Character 1 Has The Value " + (int)'1');
+        Console.WriteLine("The Character 2 Has The Value " + (int)'2');
+        Console.WriteLine("The Character $ Has The Value " + (int)'$');
+        Console.WriteLine("The Character * Has The Value " + (int)'*');
+        Console.WriteLine("The Character + Has The Value " + (int)'+');
+        Console.WriteLine("The Character / Has The Value " + (int)'/');
     }
 }
